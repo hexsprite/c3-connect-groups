4eac3b88f106c1349cfa38305ab10a98b75102d6
KayJayGoosay <kjguce@gmail.com>
2025-08-02 17:42:40 -0400

    Add visual indicator to styling branch

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -12,7 +12,7 @@ export default function Header() {
         {/* C3 Toronto Logo */}
         <div className="flex items-center">
           <div className="bg-black text-white px-4 py-2 rounded-full text-sm font-semibold tracking-wide">
-            C3 TORONTO
+            C3 TORONTO [STYLING BRANCH]
           </div>
         </div>
 
